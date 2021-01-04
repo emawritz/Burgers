d674faa122b9ac369ad4f389b6309595d4977f48
Emanuel Cejas <70048307+emawritz@users.noreply.github.com>
2021-01-04 02:36:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
